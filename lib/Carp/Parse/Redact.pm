a8d115c263c08efa5627f8a6e6d00c7c1c904971
Kate Kirby <kate@thinkgeek.com>
2012-09-17 15:45:52 -0400

    Small fix to deal with trying to redact arguments when the input is undefined.

--- lib/Carp/Parse/Redact.pm
+++ lib/Carp/Parse/Redact.pm
@@ -23,11 +23,11 @@ sensitive information contained in the function arguments for each caller.
 
 =head1 VERSION
 
-Version 1.1.1
+Version 1.1.2
 
 =cut
 
-our $VERSION = '1.1.1';
+our $VERSION = '1.1.2';
 
 
 =head1 DEFAULTS FOR REDACTING SENSITIVE DATA
